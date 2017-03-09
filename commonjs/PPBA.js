1c88dcca054d178bf124b66d3554491e6aa69ecc
andreas-trad <atrantidis@gmail.com>
2017-03-09 13:08:38 +0200

    Version 0.0.1-alpha.1

---
+++ commonjs/PPBA.js
@@ -0,0 +1,214 @@
+'use strict';
+
+var Logger = require('./modules/logger');
+var PubSub = require('./modules/pubsub');
+var Caller = require('./modules/caller');
+var Store = require('./modules/store');
+var Dom = require('./modules/dom');
+var ppbaConf = {};
+
+var afterRender = function afterRender() {
+	document.getElementById('--puresdk-apps-icon--').addEventListener('click', function (e) {
+		e.stopPropagation();
+		Dom.toggleClass(document.getElementById('--puresdk-apps-container--'), 'active');
+	});
+
+	document.getElementById('--puresdk-user-avatar--').addEventListener('click', function (e) {
+		e.stopPropagation();
+		Dom.removeClass(document.getElementById('--puresdk-apps-container--'), 'active');
+		Dom.toggleClass(document.getElementById('--puresdk-user-sidebar--'), 'active');
+	});
+
+	window.addEventListener('click', function (e) {
+		Dom.removeClass(document.getElementById('--puresdk-apps-container--'), 'active');
+		Dom.removeClass(document.getElementById('--puresdk-user-sidebar--'), 'active');
+	});
+};
+
+var PPBA = {
+	setWindowName: function setWindowName(wn) {
+		Store.setWindowName(wn);
+	},
+
+	setConfiguration: function setConfiguration(conf) {
+		Store.setConfiguration(conf);
+	},
+
+	setHTMLTemplate: function setHTMLTemplate(template) {
+		Store.setHTMLTemplate(template);
+	},
+
+	init: function init(conf) {
+		Logger.log('initializing with conf: ', conf);
+		if (conf) {
+			if (conf.headerDivId) {
+				Store.setHTMLContainer(conf.headerDivId);
+			}
+			if (conf.appsVisible !== null) {
+				Store.setAppsVisible(conf.appsVisible);
+			}
+		}
+		ppbaConf = conf;
+		return true;
+	},
+
+	authenticate: function authenticate(_success) {
+		var self = PPBA;
+		Caller.makeCall({
+			type: 'GET',
+			endpoint: Store.getAuthenticationEndpoint(),
+			callbacks: {
+				success: function success(result) {
+					Logger.log(result);
+					Store.setUserData(result);
+					self.render();
+					PPBA.getApps();
+					_success(result);
+				},
+				fail: function fail(err) {
+					window.location = Store.getLoginUrl();
+				}
+			}
+		});
+	},
+
+	authenticatePromise: function authenticatePromise() {
+		var self = PPBA;
+		return Caller.promiseCall({
+			type: 'GET',
+			endpoint: Store.getAuthenticationEndpoint(),
+			middlewares: {
+				success: function success(result) {
+					Logger.log(result);
+					Store.setUserData(result);
+					self.render();
+					PPBA.getApps();
+				}
+			}
+		});
+	},
+
+	getApps: function getApps() {
+		Caller.makeCall({
+			type: 'GET',
+			endpoint: Store.getAppsEndpoint(),
+			callbacks: {
+				success: function success(result) {
+					Store.setApps(result);
+					PPBA.renderApps(result.apps);
+				},
+				fail: function fail(err) {
+					window.location = Store.getLoginUrl();
+				}
+			}
+		});
+	},
+
+	getAvailableListeners: function getAvailableListeners() {
+		return PubSub.getAvailableListeners();
+	},
+
+	subscribeListener: function subscribeListener(eventt, funct) {
+		return PubSub.subscribe(eventt, funct);
+	},
+
+	getUserData: function getUserData() {
+		return Store.getUserData();
+	},
+
+	setInputPlaceholder: function setInputPlaceholder(txt) {
+		document.getElementById(Store.getSearchInputId()).placeholder = txt;
+	},
+
+	changeAccount: function changeAccount(accountId) {
+		Caller.makeCall({
+			type: 'GET',
+			endpoint: Store.getSwitchAccountEndpoint(accountId),
+			callbacks: {
+				success: function success(result) {
+					window.location = '/';
+				},
+				fail: function fail(err) {
+					alert('Sorry, something went wrong with your request. Plese try again');
+				}
+			}
+		});
+	},
+
+	renderApps: function renderApps(apps) {
+		var appTemplate = function appTemplate(app) {
+			return '\n\t\t\t\t<a href="#" style="background: #' + app.color + '"><i class="' + app.icon + '"></i></a>\n\t\t\t\t<span class="bac--app-name">' + app.name + '</span>\n\t\t\t\t<span class="bac--app-description">' + app.descr + '</span>\n\t\t\t';
+		};
+
+		var _loop = function _loop(i) {
+			var app = apps[i];
+			var div = document.createElement("div");
+			div.className = "bac--apps";
+			div.innerHTML = appTemplate(app);
+			div.onclick = function (e) {
+				e.preventDefault();
+				window.location = app.application_url;
+			};
+			document.getElementById("--puresdk-apps-container--").appendChild(div);
+		};
+
+		for (var i = 0; i < apps.length; i++) {
+			_loop(i);
+		}
+	},
+
+	renderUser: function renderUser(user) {
+		var userTemplate = function userTemplate(user) {
+			return '\n\t\t\t\t<div class="bac--user-image"><i class="fa fa-camera"></i></div>\n\t\t\t\t<div class="bac--user-name">' + user.firstname + ' ' + user.lastname + '</div>\n\t\t\t\t<div class="bac--user-email">' + user.email + '</div>\n\t\t\t';
+		};
+		var div = document.createElement('div');
+		div.className = "bac--user-sidebar-info";
+		div.innerHTML = userTemplate(user);
+		document.getElementById('--puresdk-user-details--').appendChild(div);
+		document.getElementById('--puresdk-user-avatar--').innerHTML = user.firstname.charAt(0) + user.lastname.charAt(0);
+	},
+
+	renderAccounts: function renderAccounts(accounts) {
+		var accountsTemplate = function accountsTemplate(account) {
+			return '\n\t\t\t\t<img src="http://lorempixel.com/40/40" alt="">\n\t\t\t\t<div class="bac-user-app-details">\n\t\t\t\t\t <span>' + account.name + '</span>\n\t\t\t\t\t <span>15 team members</span>\n\t\t\t\t</div>\n\t\t\t';
+		};
+
+		var _loop2 = function _loop2(i) {
+			var account = accounts[i];
+			var div = document.createElement('div');
+			div.className = 'bac--user-list-item';
+			div.innerHTML = accountsTemplate(account);
+			div.onclick = function (e) {
+				e.preventDefault();
+				PPBA.changeAccount(account.sfid);
+			};
+			document.getElementById('--puresdk-user-businesses--').appendChild(div);
+		};
+
+		for (var i = 0; i < accounts.length; i++) {
+			_loop2(i);
+		}
+	},
+
+	render: function render() {
+		var whereTo = document.getElementById(Store.getHTLMContainer());
+		if (whereTo === null) {
+			Logger.error('the container with id "' + whereTo + '" has not been found on the document. The library is going to create it.');
+			var div = document.createElement('div');
+			div.id = Store.getHTLMContainer();
+			div.style.width = '100%';
+			div.style.height = '60px';
+			document.body.insertBefore(div, document.body.firstChild);
+			whereTo = document.getElementById(Store.getHTLMContainer());
+		}
+		whereTo.innerHTML = Store.getHTML();
+		PPBA.renderUser(Store.getUserData().user);
+		PPBA.renderAccounts(Store.getUserData().user.accounts);
+		if (Store.getAppsVisible() === false) {
+			document.getElementById('--puresdk-apps-section--').style = "display:none";
+		}
+		afterRender();
+	}
+};
+
+module.exports = PPBA;
